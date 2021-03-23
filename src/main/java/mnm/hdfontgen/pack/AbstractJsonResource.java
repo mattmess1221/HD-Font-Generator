af13ef48837a15819a2ac1225e9ff5e472c2029d
Matthew Messinger <mattmess1221@gmail.com>
2021-03-22 23:19:27 -0400

    Pretty print json

--- src/main/java/mnm/hdfontgen/pack/AbstractJsonResource.java
+++ src/main/java/mnm/hdfontgen/pack/AbstractJsonResource.java
@@ -1,13 +1,14 @@
 package mnm.hdfontgen.pack;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public abstract class AbstractJsonResource implements Resource {
-    private static final Gson gson = new Gson();
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     protected Object getRoot() {
         return this;
